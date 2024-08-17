5e7e338d4cc169405886a0c673d7d5e98a06cd09
Chí Đạt <153970055+Dat213DZ@users.noreply.github.com>
2024-08-17 23:43:46 +0700

    Excute crud ProductController

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,15 +1,130 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using testthuctap.data.migrations;
 namespace testthuctap.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ProductController : ControllerBase
     {
-        [HttpGet]
-        public async Task<IActionResult> GetAll()
+        private readonly DBContextUser _context;
+
+        public ProductController(DBContextUser context)
         {
-            return Ok("ProductController");
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Product.ToListAsync();
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Product.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // PUT: api/Products/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        public class ProductAddDto
+        {
+            public int ProductId { get; set; }
+            public string ProductName { get; set; } = null!;
+            public decimal Price { get; set; }
+            public int CategoryId { get; set; }
+        }
+        public class ProductUpdateDto
+        {
+            public string ProductName { get; set; } = null!;
+            public decimal Price { get; set; }
+            public int CategoryId { get; set; }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(int id, ProductUpdateDto productDto)
+        {
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            product.ProductName = productDto.ProductName;
+            product.Price = productDto.Price;
+            product.CategoryID = productDto.CategoryId;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Products
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(ProductAddDto productDto)
+        {
+            var product = new Product
+            {
+                ProductName = productDto.ProductName,
+                Price = productDto.Price,
+                CategoryID = productDto.CategoryId
+            };
+
+            _context.Product.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = product.ProductID }, product);
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Product.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Product.Any(e => e.ProductID == id);
         }
     }
 }
