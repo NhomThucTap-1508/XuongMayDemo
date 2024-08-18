377b72aab509561bf85823e395ea7d04e262dc2a
Chí Đạt <153970055+Dat213DZ@users.noreply.github.com>
2024-08-18 23:38:56 +0700

    Fix put category and update user admin

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,14 +18,16 @@ public class CategoryController : ControllerBase
     }
 
     // GET: api/Categories: Lấy toàn bộ danh sách loại sản phẩm
-    [HttpGet]
+    [HttpGet("GetAllCategory")]
+    [Authorize(Roles = "Admin")]
     public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
     {
         return await _context.Category.ToListAsync();
     }
 
     // GET: api/Categories/5: Lấy 1 trong các loại sản phẩm
-    [HttpGet("{id}")]
+    [HttpGet("Id")]
+    [Authorize(Roles = "Admin,LineLeader")]
     public async Task<ActionResult<Category>> GetCategory(int id)
     {
         var category = await _context.Category.FindAsync(id);
@@ -45,43 +48,29 @@ public class CategoryController : ControllerBase
 
         public string CategoryName { get; set; } = null!;
     }
-    [HttpPut("{id}")]
+    [HttpPut("UpdateCategory")]
+    [Authorize(Roles = "Admin")]
     public async Task<IActionResult> PutCategory(int id, CategoryAddDto categoryDto)
     {
-        var category = new Category
+        var category = await _context.Category.FindAsync(id);
+        if (category == null)
         {
-            CategoryID = categoryDto.CategoryId,
-            CategoryName = categoryDto.CategoryName
-        };
-        if (id != category.CategoryID)
-        {
-            return BadRequest();
+            return NotFound();
         }
-
-        _context.Entry(category).State = EntityState.Modified;
-
-        try
+        category.CategoryName = categoryDto.CategoryName;
+        _context.Category.Update(category);
+        int rowChange = await _context.SaveChangesAsync();
+        if (rowChange > 0)
         {
-            await _context.SaveChangesAsync();
+            return Ok("Update category successful!!!");
         }
-        catch (DbUpdateConcurrencyException)
-        {
-            if (!CategoryExists(id))
-            {
-                return NotFound();
-            }
-            else
-            {
-                throw;
-            }
-        }
-
-        return NoContent();
+        return StatusCode(500, "Update category unsuccessful!!!");
     }
 
     // POST: api/Categories: Cập nhật loại sản phẩm
     // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-    [HttpPost]
+    [HttpPost("CreateCategory")]
+    [Authorize(Roles = "Admin")]
     public async Task<ActionResult<Category>> PostCategory(CategoryAddDto categoryDto)
     {
         var category = new Category
@@ -90,13 +79,17 @@ public class CategoryController : ControllerBase
             CategoryName = categoryDto.CategoryName
         };
         _context.Category.Add(category);
-        await _context.SaveChangesAsync();
-
-        return CreatedAtAction("GetCategory", new { id = category.CategoryID }, category);
+        int rowChange = await _context.SaveChangesAsync();
+        if (rowChange > 0) {
+            return Ok("Create category sucessful!!!");
+        }
+        return StatusCode(500, "Create category unsuccessful!!!");
+        
     }
 
     // DELETE: api/Categories/5: Xóa 1 loại sản phẩm
-    [HttpDelete("{id}")]
+    [HttpDelete("DeleteCategory")]
+    [Authorize(Roles = "Admin,LineLeader")]
     public async Task<IActionResult> DeleteCategory(int id)
     {
         var category = await _context.Category.FindAsync(id);
@@ -108,7 +101,7 @@ public class CategoryController : ControllerBase
         _context.Category.Remove(category);
         await _context.SaveChangesAsync();
 
-        return NoContent();
+        return StatusCode(500,"Delete successful!!!");
     }
 
     private bool CategoryExists(int id)
